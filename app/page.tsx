ebff0227ec39d587a92a2369c594502bf49bc873
realSJ30 <realsjmoraga@gmail.com>
2023-07-23 21:31:05 +0800

    fix: dynamic server usage searchParams.userId

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsParams } from "./actions/getListings";
 import Container from "./components/Container";
@@ -16,7 +17,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   if (listings.length === 0) {
     return <EmptyState showReset />;
   }
-  
+
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
